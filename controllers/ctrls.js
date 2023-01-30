4192e96a8571064d06f85dc25b991bab879a0000
ItVal <valentinhonvj1@gmail.com>
2023-01-30 14:21:37 +0100

    creation et definition de controllers

---
+++ controllers/ctrls.js
@@ -0,0 +1,17 @@
+
+// get all
+const getAll = (req, res) => {};
+
+// get one
+const getOne = (req, res) => {};
+
+// create one
+const createOne = (req, res) => {};
+
+// update one
+const updateOne = (req, res) => {};
+
+// delete one
+const deleteOne = (req, res) => {};
+
+export { getAll, getOne, createOne, updateOne, deleteOne };
