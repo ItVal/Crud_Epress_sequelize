d7ce02e3e15baf86cdf57d67d5e6d29da6929dca
ItVal <valentinhonvj1@gmail.com>
2023-01-30 13:52:33 +0100

    creation et definition de routes

---
+++ routes/routes.js
@@ -0,0 +1,10 @@
+import {Router} from 'express';
+
+const router = Router();
+
+
+router.get('/getAll', "") ;
+router.get('/getOne/:id', "") ;
+router.post('/createOne', "") ;
+router.put('/updateOne/:id', "") ;
+router.get('/deleteOne/:id', "") ;
