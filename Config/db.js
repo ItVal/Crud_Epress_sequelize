deb4cddece20b07d3aab64aa913c59631344382b
ItVal <valentinhonvj1@gmail.com>
2023-01-31 22:12:27 +0100

    install dotenv & use environment variables

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,9 @@
 //connexion à la base de données
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv'
+dotenv.config();
 
-export default new Sequelize("crud", "jonas", "KDA1994+Val", {
+export default new Sequelize("crud", process.env.DB_USERNAME,  process.env.DB_PASSWORD, {
   dialect: "mysql",
   host: "localhost",
 });
