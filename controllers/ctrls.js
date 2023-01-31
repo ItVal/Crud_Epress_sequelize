d0423c47cb1f743ca3ecbc1472a5a51cbfcbcf95
ItVal <valentinhonvj1@gmail.com>
2023-01-31 20:31:40 +0100

    get all  product function

--- controllers/ctrls.js
+++ controllers/ctrls.js
@@ -8,16 +8,26 @@ const createOne = (req, res) => {
     if (error) return res.status(401).json(error.detail[0].message)
     Product.create({title:body.title, price:body.price, description:body.description})
     .then(() =>{
-        res.status(200).json({msg : "product created"})
+        res.status(201).json({msg : "product created"})
     })
     .catch(error => res.status(500).json(error))
 };
 
 // get all
-const getAll = (req, res) => {};
+const getAll = (req, res) => {
+    Product.findAll({
+        attributes: {exclude : ["createdAt", "updatedAt"]}
+    })
+    .then((products) =>{
+        res.status(200).json(products)
+    })
+    .catch(error => res.status(500).json(error))
+};
 
 // get one
-const getOne = (req, res) => {};
+const getOne = (req, res) => {
+   
+};
 
 
 // update one
